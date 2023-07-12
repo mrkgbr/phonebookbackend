a5c0bbf951f1d484a555c7566b72f33e656a6e9d
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-12 13:07:00 +0200

    Updated to 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -23,6 +25,10 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 100000000000);
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -58,6 +64,26 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
