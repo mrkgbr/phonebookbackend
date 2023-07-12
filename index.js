4bdc1c97a18c7f3085758ba3dfe0e8f3268b1cf3
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-12 18:31:45 +0200

    Updated to 3.8

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(function (tokens, req, res) {
+    if (req.method === "POST") {
+      const body = JSON.stringify(req.body);
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        body,
+      ].join(" ");
+    }
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
@@ -68,8 +95,6 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-
-  console.log(body.name);
 
   // returns if name or number missing
   if (!body.name || !body.number) {
