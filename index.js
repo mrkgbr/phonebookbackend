443af3ff51c70c946bb620cc2f3db2ec3ceb53a8
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-14 08:20:02 +0200

    added build and configured links and proxy

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
+app.use(express.static("build"));
 app.use(express.json());
+
 app.use(
   morgan(function (tokens, req, res) {
     if (req.method === "POST") {
@@ -126,7 +131,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
